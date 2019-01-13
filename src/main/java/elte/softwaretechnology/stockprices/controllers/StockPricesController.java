60a5a829391d258faeaa6dd34251ac5862106352
David Kelemen <david.kelemen@ericsson.com>
2019-01-13 19:04:27 +0100

    Configure query dates from application.properties

--- src/main/java/elte/softwaretechnology/stockprices/controllers/StockPricesController.java
+++ src/main/java/elte/softwaretechnology/stockprices/controllers/StockPricesController.java
@@ -1,5 +1,20 @@
 package elte.softwaretechnology.stockprices.controllers;
 
+import static elte.softwaretechnology.stockprices.collectors.DataCollectorType.NewYorkTimes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import elte.softwaretechnology.stockprices.collectors.DataCollector;
 import elte.softwaretechnology.stockprices.collectors.DataCollectorFactory;
 import elte.softwaretechnology.stockprices.data.model.QueryParameter;
@@ -7,18 +22,6 @@ import elte.softwaretechnology.stockprices.data.model.QueryResult;
 import elte.softwaretechnology.stockprices.data.service.QueryResultService;
 import elte.softwaretechnology.stockprices.data.service.StockDataService;
 import elte.softwaretechnology.stockprices.parsers.implementations.DailyStockDataParser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.time.LocalDate;
-import java.util.Scanner;
-
-import static elte.softwaretechnology.stockprices.collectors.DataCollectorType.NewYorkTimes;
 
 @RestController
 @Transactional
@@ -32,9 +35,15 @@ public class StockPricesController { //todo another controller for different pur
 	@Autowired
 	private StockDataService stockDataService;
 
+	@Value("${new.york.times.start.date}")
+	private String startDate;
+
+	@Value("${new.york.times.end.date}")
+	private String endDate;
+
 	@GetMapping(path = "/")
 	public String getHome() {
-
+		System.out.println(startDate + " " + endDate);
 		return "index";
 	}
 
@@ -49,8 +58,7 @@ public class StockPricesController { //todo another controller for different pur
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(new File("src/main/resources/AAPL.csv"));
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		scanner.nextLine(); //first row is header
@@ -58,28 +66,33 @@ public class StockPricesController { //todo another controller for different pur
 			stockDataService.save(dailyStockDataParser.parseStockData(scanner.nextLine()));
 		}
 
-		return "index"; //todo create a proper page
+		return "index"; //TODO create a proper page
 	}
 
 	@GetMapping(path = "/newyorktimes")
 	public String getNewYorkTimesApi(Model model) {
 		DataCollector dataCollector = dataCollectorFactory.createOrRetrieveDataCollector(NewYorkTimes);
 		model.addAttribute("key", dataCollector.getKey());
-		QueryParameter queryParameter = new QueryParameter(LocalDate.of(2009, 1, 1), LocalDate.of(2009, 1, 9))
-						.addMayHaveKeyWord("IOS")
-						.addMayHaveKeyWord("OS X")
-						.addMayHaveKeyWord("IPAD")
-						.addMayHaveKeyWord("MAC")
-						.addMayHaveKeyWord("STEVE JOBS")
-						.addMayHaveKeyWord("SMART PHONE")
-						.addMayHaveKeyWord("ITUNES")
-						.addMayHaveKeyWord("APP STORE")
-						.addMayHaveKeyWord("IPHONE")
-						.addMayHaveKeyWord("AAPL")
-						.addMustHaveKeyWord("APPLE");
+		QueryParameter queryParameter = new QueryParameter(getDate(startDate), getDate(endDate))
+				.addMayHaveKeyWord("IOS")
+				.addMayHaveKeyWord("OS X")
+				.addMayHaveKeyWord("IPAD")
+				.addMayHaveKeyWord("MAC")
+				.addMayHaveKeyWord("STEVE JOBS")
+				.addMayHaveKeyWord("SMART PHONE")
+				.addMayHaveKeyWord("ITUNES")
+				.addMayHaveKeyWord("APP STORE")
+				.addMayHaveKeyWord("IPHONE")
+				.addMayHaveKeyWord("AAPL")
+				.addMustHaveKeyWord("APPLE");
 		QueryResult queryResult = dataCollector.queryContent(queryParameter);
 		queryResultService.save(queryResult);
 		model.addAttribute("html", "done");
 		return NewYorkTimes.getId();
+	}
+
+	private LocalDate getDate(String date) {
+		int[] dateArray = Arrays.asList(date.split("-")).stream().mapToInt(Integer::parseInt).toArray();
+		return LocalDate.of(dateArray[0], dateArray[1], dateArray[2]);
 	}
 }
